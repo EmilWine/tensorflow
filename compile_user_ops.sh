9e81cae27165df81227445b59bdf92f5e77ff071
dor <dor@insoundz.com>
2019-10-29 10:52:31 +0000

    Added RankOneUpdate op (Not in place of a variable)

--- compile_user_ops.sh
+++ compile_user_ops.sh
@@ -13,7 +13,7 @@ combinediff ./third_party/eigen3/gpu_packet_math.patch ./third_party/eigen3/tens
 MKLML_PREFACE="mklml_lnx_2019.0.5.20190502"
 echo "######   Preparing MKL Library for compilation   ######"
 mkdir -p ./mklml
-wget https://github.com/intel/mkl-dnn/releases/download/v0.21/${MKLML_PREFACE}.tgz -O mklml.tgz
+wget -N -nc https://github.com/intel/mkl-dnn/releases/download/v0.21/${MKLML_PREFACE}.tgz -O mklml.tgz
 tar -C ./mklml -xzvf ./mklml.tgz
 ln -s $MKLROOT/include/* mklml/${MKLML_PREFACE}/include/ 2>/dev/null
 ln -s $MKLROOT/lib/intel64/* mklml/${MKLML_PREFACE}/lib/ 2>/dev/null 
@@ -23,4 +23,12 @@ export TF_MKL_ROOT="${PWD}/mklml/${MKLML_PREFACE}"
 
 JOBS=$(($(grep -c processor /proc/cpuinfo)-2))
 #--cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0"
-bazel build --jobs ${JOBS} -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --config=mkl --cxxopt="-DEIGEN_MKL_DEFAULT" --cxxopt="-DEIGEN_USE_MKL_ALL" -s //tensorflow/core/user_ops:emilsky_op.so
+
+
+DBG="--compilation_mode=dbg -c dbg --copt=-g --copt=-O0 --cxxopt=-O0 --cxxopt=-g --strip=never -s"
+bazel build --jobs ${JOBS} -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --config=mkl --cxxopt="-DEIGEN_MKL_DEFAULT" --cxxopt="-DEIGEN_USE_MKL_ALL" $DBG //tensorflow/core/user_ops:rankoneupdate_op.so
+
+#bazel build --jobs ${JOBS} -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --config=mkl --cxxopt="-DEIGEN_MKL_DEFAULT" --cxxopt="-DEIGEN_USE_MKL_ALL" $DBG //tensorflow/core/user_ops:time_two_kernels.so &&
+#bazel build --jobs ${JOBS} -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --config=mkl --cxxopt="-DEIGEN_MKL_DEFAULT" --cxxopt="-DEIGEN_USE_MKL_ALL" -s //tensorflow/core/user_ops:emilsky_op.so
+#bazel build --jobs ${JOBS} -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --config=mkl --cxxopt="-DEIGEN_MKL_DEFAULT" --cxxopt="-DEIGEN_USE_MKL_ALL" $DBG //tensorflow/core/user_ops:assign_variable_test2_op.so &&
+#bazel build --jobs ${JOBS} -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --config=mkl --cxxopt="-DEIGEN_MKL_DEFAULT" --cxxopt="-DEIGEN_USE_MKL_ALL" $DBG //tensorflow/core/user_ops:assign_variable_test_op.so 
